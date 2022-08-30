52624ff085191f8bbbdff8a31280652e1564ac08
Jason <jason.onggo@tempatkerja.com>
2022-08-30 05:42:53 +0000

    move comments

--- texttoparsed/texttoparsed.go
+++ texttoparsed/texttoparsed.go
@@ -42,7 +42,6 @@ func FindAllSubmatch(input []byte) (output [][][]byte, err error) {
 			// if the Group DATE is empty then it is a subline of a transaction. So, append the matches to the previous element.
 			if len(matches[matchIndex][0]) != 0 && len(matches[matchIndex][1]) == 0 {
 
-				// a transaction may continue on the next page. So, concatenate the matches to the output.
 				for submatchIndex, submatch := range matches[matchIndex] {
 					// submatchIndex == 0 because func (*regexp.Regexp).FindAllSubmatch matches the line at index 0. So, ignore it.
 					// len(submatch) == 0 because the regex matches empty column. So, ignore it.
@@ -50,6 +49,7 @@ func FindAllSubmatch(input []byte) (output [][][]byte, err error) {
 						continue
 					}
 
+					// a transaction may continue on the next page. So, concatenate the matches to the output.
 					if matchIndex == 0 {
 						transactionIndex := len(output) - 1
 						output[transactionIndex][submatchIndex] = append(output[transactionIndex][submatchIndex], []byte("\n")...)
